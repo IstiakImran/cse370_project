8e4761a9db41cb3f6d66912e5de57499add39e57
IstiakImran <toufique.hassan.71@gmail.com>
2024-07-04 19:41:58 +0600

    port changed again to check

--- ridesharing-backend/src/config/database.js
+++ ridesharing-backend/src/config/database.js
@@ -4,24 +4,14 @@ dotenv.config()
 
 //Create a connection pool to the database
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-  }).promise();
-  
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error connecting to the database:', err.message);
-    } else {
-      console.log('Connected to the database');
-      connection.release();
-    }
-  });
-  
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}).promise();
 
 
 
